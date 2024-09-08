65a67581913b36c7468758757c3aee10fbd892d3
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-08 22:06:27 +0600

    object properties

---
+++ object_properties.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'Tod',
+    age: 25,
+    profession: 'teacher',
+    married: true,
+    salary: 10000
+}
+
+
+// dot notation method
+console.log(person.salary);
+console.log(person.profession);
+const salary = person.salary;
+console.log(salary);
