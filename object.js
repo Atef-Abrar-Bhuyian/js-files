897fdb64ffd31786d07014581b8a0ba7673362bd
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-08 21:59:27 +0600

    declare an object

---
+++ object.js
@@ -0,0 +1,27 @@
+
+// Primitive,
+// basic, primary data types
+const age = 24;
+const varsity = 'Port City';
+const isPassed = true;
+
+// object    
+// object types is non-primitive
+
+const bottle = {
+    brand: 'Apple',
+    price: 45,
+    color: 'white'
+}
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda begum',
+    examDate: '30 feb',
+    chapter: ['first chapter', 'second chapter', 'third'],
+    exams: {
+        name: 'final exam',
+        marks: 100
+    }
+}
+
