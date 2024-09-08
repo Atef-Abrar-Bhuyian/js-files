ff20bfed0b3c3ccbf805d29899922b2926ae9bc2
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-08 21:17:55 +0600

    reverse method- for loop

--- loop/reverse.js
+++ loop/reverse.js
@@ -1,10 +1,18 @@
 const sentence = 'I am learning web development';
 
-// for loop
+// for of method
 let reverse = '';
 for (const letter of sentence){
     // console.log(letter);
     reverse =letter + reverse;
 }
 console.log(reverse);
+
+// for loop method
+let rev='';
+for (let i=0; i< sentence.length; i++){
+    let letter=sentence[i];
+    rev = letter + rev;
+}
+console.log(rev);
 
