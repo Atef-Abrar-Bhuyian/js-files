6a14bc45dd3ed0f657b80fdf9acd16d1e651942d
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-09 21:33:27 +0600

    boolean,array,object types parameter can use in fucntion

--- function/various_parameter.js
+++ function/various_parameter.js
@@ -2,6 +2,7 @@
  * For a given string tell me whether it has even numbers of character or not 
  */
 
+// String Types Parameter
 function evenSizedString(str){
     const size = str.length;
     if( str.length % 2 ===0){
@@ -15,6 +16,7 @@ function evenSizedString(str){
 evenSizedString("Chittagong");
 
 
+// Boolean types parameter
 function doubleOrTripple(number, doDouble){
     if(doDouble){
         const result = number * 2;
@@ -28,3 +30,28 @@ function doubleOrTripple(number, doDouble){
 
 console.log(doubleOrTripple(12, true));
 console.log(doubleOrTripple(10, false));
+
+
+// Array types Parameter
+
+function numberOfElements(numbers){
+    const totalLength = numbers.length;
+    return totalLength;
+}
+
+console.log(numberOfElements([12,52,31,96,4,5]));
+
+
+// object types parameter
+function getAge(x){
+    const age = x.age;
+    return age;
+}
+
+let person = {
+    age: 25,
+    married: true,
+    office: 'Dhaka',
+}
+
+console.log(getAge(person));
