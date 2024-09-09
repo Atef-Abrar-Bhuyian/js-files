396449019a78cad16b6e130da3b4a943a1a2cee7
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-09 19:35:05 +0600

    function declare and call

---
+++ function/function.js
@@ -0,0 +1,40 @@
+// Function's basic structure
+
+
+// function name(){
+
+// }    
+
+const number = 45;
+
+
+// Function declare
+function fanOffKor(){
+    console.log("Bosha theke uthe fan off kor");
+    console.log("Pani niye aai");
+    console.log("Thank you");
+}
+
+// Call the function
+fanOffKor();
+
+// Function declare
+function brushTeeth(){
+    console.log("Pick up the brush");
+    console.log("proper way te koro");
+    console.log("jaa iccha koro");
+}
+
+// Call the function
+
+brushTeeth();   
+
+
+// two number's sum with function
+function sum(x,y){
+    const result=x+y;
+    return result;
+}
+
+const total = sum(100,200);
+console.log(total);
