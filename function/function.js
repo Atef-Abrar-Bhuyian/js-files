75f4913e530e44f26978257a623f635d62adc626
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-09 19:47:53 +0600

    function's parameter

--- function/function.js
+++ function/function.js
@@ -32,9 +32,36 @@ brushTeeth();
 
 // two number's sum with function
 function sum(x,y){
+    // Here x,y is parameter
     const result=x+y;
     return result;
 }
 
 const total = sum(100,200);
 console.log(total);
+
+
+
+// Math Square Problem fix with function
+
+function square(x){
+    const result= x*x;
+    return result; 
+}
+console.log(square(4))
+// const squareResult = square(5);
+// console.log(squareResult);
+
+
+
+// sum problem fix with function parameter
+
+function add(x,y){
+    const sum= x+y;
+    console.log(sum);
+}
+
+add(5,6);
+add(65,6);
+add(96,98);
+add(96,4);
